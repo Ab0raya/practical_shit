c3b7ae5413cb14eafbf866de2a78ad2739bc1eb1
Aburaya <mhmdsayed.152005@gmail.com>
2025-05-17 04:49:37 +0300

    replace filter chip

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,62 @@
 // travel main
-
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:practical_shit/travel/view/welcome_screen.dart';
-
-import 'travel/controller/city_controller.dart';
-
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Travel App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      initialBinding: BindingsBuilder(() {
-        Get.put(CityController());
-      }),
-      home: const WelcomeScreen(),
-    );
-  }
-}
-
-
-// coffee main
+//
 // import 'package:flutter/material.dart';
 // import 'package:get/get.dart';
-// import 'package:practical_shit/coffee/view/splahs_view.dart';
+// import 'package:practical_shit/travel/view/welcome_screen.dart';
+//
+// import 'travel/controller/city_controller.dart';
+//
 //
 // void main() {
 //   runApp(const MyApp());
 // }
 //
 // class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+//   const MyApp({Key? key}) : super(key: key);
 //
 //   @override
 //   Widget build(BuildContext context) {
 //     return GetMaterialApp(
+//       title: 'Travel App',
 //       debugShowCheckedModeBanner: false,
-//       title: 'Flutter Demo',
 //       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
 //       ),
-//       home: const SplashView(),
+//       initialBinding: BindingsBuilder(() {
+//         Get.put(CityController());
+//       }),
+//       home: const WelcomeScreen(),
 //     );
 //   }
 // }
+
+
+// coffee main
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:practical_shit/coffee/view/splahs_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
 
 // burger main :
 
